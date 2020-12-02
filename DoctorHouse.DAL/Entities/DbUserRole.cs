c0ca9c1c946c51aea0610e1b68b16076e3695444
BohdanZyshchyk <bohdan.zyshchuk@zagravagames.com>
2020-12-02 19:53:57 +0200

    added DAL and migrations

---
+++ DoctorHouse.DAL/Entities/DbUserRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DoctorHouse.DAL.Entities
+{
+    public class DbUserRole : IdentityUserRole<long>
+    {
+        public virtual DbUser User { get; set; }
+        public virtual DbRole Role { get; set; }
+
+    }
+}
